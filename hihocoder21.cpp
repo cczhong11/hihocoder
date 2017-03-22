e7fd66bebaef69ed8d99f9664a88dd7252925aa3
cczhong11 <tczhong24@gmail.com>
2017-03-22 12:27:32 +0800

    finally ac hihocoder21 so happy. about 7h

--- hihocoder21.cpp
+++ hihocoder21.cpp
@@ -16,8 +16,8 @@ int getid(int x)
 }  
 void push_up(int rt)
 {
-    if(c[rt<<1]!=c[rt<<1|1])
-        c[rt]=-1;
+    if((c[rt<<1]!=c[rt<<1|1])||(c[rt<<1|1]!=c[rt]))
+        {c[rt]=-1;lazy[rt] = 0;}
 }
 void push_down(int rt)
 {
@@ -26,6 +26,7 @@ void push_down(int rt)
         lazy[rt<<1] = lazy[rt<<1|1] = lazy[rt];
         c[rt<<1] = c[rt<<1|1] = c[rt];
         lazy[rt] = 0;
+        c[rt]=-1;
     }
 }
 void build(int l, int r)
@@ -38,6 +39,12 @@ void build(int l, int r)
 }
 void update(int L, int R, int c0, int l, int r, int rt)
 {
+    if(l==r) {
+        lazy[rt] = c0;
+        c[rt] = c0;
+        return;
+    }
+    //printf("%d %d %d %d %d %d \n",rt,c[rt],l,r,L,R);
     if(L<=l && r<=R)
     {
         lazy[rt] = c0;
@@ -46,16 +53,39 @@ void update(int L, int R, int c0, int l, int r, int rt)
     }
     push_down(rt);
     int m = (l+r) >> 1;
-    if(L<=m) update(L,R,c0,l,m,rt<<1);
-    if(m< R) update(L,R,c0,m+1,r,rt<<1|1);
+    if(L<m) update(L,R,c0,l,m,rt<<1);
+    if(m<R) update(L,R,c0,m,r,rt<<1|1);
     push_up(rt);
+}
+void query(int l,int r,int rt)
+{
+    if(l+1>=r) {
+       if(c[rt]!=-1&&c[rt]!=0)
+       ss.insert(c[rt]);
+       //printf("%d %d \n",rt,c[rt]);
+        return;
+    }
+     //printf("%d %d %d %d  \n",rt,c[rt],l,r);
+    if(lazy[rt]&&c[rt]!=-1&&c[rt]!=0)
+   {
+       //printf("%d %d \n",rt,c[rt]);
+       ss.insert(c[rt]);
+       return;
+   }
+   push_down(rt);
+   int m = (l+r) >> 1;
+   query(l,m,rt<<1);
+   query(m,r,rt<<1|1);
+    return;
+    
 }
 int query(int siz)
 {
     ss.clear(); 
     for(int i = 1; i < siz+1;i++)
     {
-        if(c[i]!=-1)
+        //printf("%d %d\n",i,c[i]);
+        if(c[i]!=-1||c[i]!=0)
             ss.insert(c[i]);
     }
     return ss.size();
@@ -63,7 +93,7 @@ int query(int siz)
 int main()
 {
     int N,M;  
-    freopen("in21.txt", "r", stdin);
+    //freopen("in21.txt", "r", stdin);
     //memset(tree,0,sizeof(tree));  
     
     v.clear();   
@@ -78,11 +108,13 @@ int main()
     sort(v.begin(),v.end());  
     v.erase(unique(v.begin(),v.end()),v.end());  
     int N2=v.size();  
-    build(1,2*N2);
+    build(0,2*N2);
     for(int i=0;i<N;i++)  
     {  
-        update(getid(index_L[i]),getid(index_R[i]),i+1,1,M,1);  
+        update(getid(index_L[i]),getid(index_R[i]),i+1,0,N2,1);  
     }
-    int result = query(2*N2);
+    //printf("---------------------------");
+    query(0,N2,1);
+    int result = ss.size();//query(2*N2);
     printf("%d\n", result);
 }
